0103bbea948446c25a6ea772bedc5caf177d557e
dqwdd-mega <dqwdd@megastudy.net>
2026-02-26 16:46:50 +0900

    feat : Hilt 적용

--- app/src/main/java/com/example/taskdeep/DeepFineApplication.kt
+++ app/src/main/java/com/example/taskdeep/DeepFineApplication.kt
@@ -1,5 +1,7 @@
 package com.example.taskdeep
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class DeepFineApplication : Application()
--- app/src/main/java/com/example/taskdeep/MainActivity.kt
+++ app/src/main/java/com/example/taskdeep/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.taskdeep.ui.theme.TaskDeep20260226Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
