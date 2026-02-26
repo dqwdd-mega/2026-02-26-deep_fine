1fccb886cf21600844d901044a093fef9cda823e
dqwdd-mega <dqwdd@megastudy.net>
2026-02-26 17:02:06 +0900

    feat : 로그인 화면 Route, ViewModel 세팅

---
+++ app/src/main/java/com/example/taskdeep/ui/LoginRoute.kt
@@ -0,0 +1,47 @@
+package com.example.taskdeep.ui
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+
+@Composable
+fun LoginRoute(
+    viewModel: LoginViewModel = hiltViewModel()
+) {
+    val state by viewModel.state.collectAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        viewModel.sideEffect.collectLatest { sideEffect ->
+            when (sideEffect) {
+                is LoginContract.SideEffect.ShowToast -> {
+                    Toast.makeText(context, sideEffect.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    LoginScreen()
+}
+
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "ExchangeRoute",
+            fontSize = 24.sp
+        )
+    }
+}
---
+++ app/src/main/java/com/example/taskdeep/ui/LoginViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.taskdeep.ui
+
+import com.example.taskdeep.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+) : BaseViewModel<LoginContract.LoginState, LoginContract.Event, LoginContract.SideEffect>() {
+
+    override val _state = MutableStateFlow(LoginContract.LoginState())
+
+    override suspend fun handleEvent(event: LoginContract.Event) {
+        when (event) {
+            else -> {}
+        }
+    }
+}
